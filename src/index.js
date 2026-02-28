ad5d4e9ebe31a8dfbf4b1b919cab6a4764cb8780
ARpatel-ARP <rautanky407@gmail.com>
2026-02-28 07:39:03 +0530

    initialize mongoose connection with error handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+( async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+        
+    }
+  
+}) ()
