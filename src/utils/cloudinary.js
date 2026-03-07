1b3f1d1fc8ad9c2c2567f2957219fa35cf6389e0
ARpatel <rautanky407@gmail.com>
2026-03-07 08:53:43 +0530

    fix: resolve user registration issues, update MongoDB connection string, and improve user model methods

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,7 +14,8 @@ import fs from "fs"
                 resource_type: "auto"
             })
                 
-            console.log("file is uploaded on cloudinary", response.url);
+            //console.log("file is uploaded on cloudinary", response.url);
+            fs.unlinkSync(localFilePath)
             return response;
         } catch (error) {
             fs.unlinkSync(localFilePath) // remove the locally saved temp file as the upload operation got failed
